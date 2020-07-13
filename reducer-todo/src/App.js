cba47a6c3b295ef69840264beaa11bdead24c377
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-13 15:30:45 -0500

    ✅ STEP 2: set up state in App + render list of todos

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
+import { todoReducer, initialState } from './reducers/todoReducer';
+import TodosList from './components/TodosList';
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
   return (
     <div className='App'>
       <h1>Reducer Pattern Todo App</h1>
+      <TodosList todos={state} />
     </div>
   );
 }
