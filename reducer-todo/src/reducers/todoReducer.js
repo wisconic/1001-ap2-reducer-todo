d3f731d74a1364008553c0d467e9bba49a5002f9
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-13 15:00:31 -0500

    🐛 FIX: made initial state an [] of {}s

--- reducer-todo/src/reducers/todoReducer.js
+++ reducer-todo/src/reducers/todoReducer.js
@@ -1,8 +1,10 @@
-export const initialState = {
-  item: 'Learn about reducers',
-  completed: false,
-  id: 3892987589,
-};
+export const initialState = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589,
+  },
+];
 
 export const todoReducer = (state, action) => {
   switch (action.type) {
