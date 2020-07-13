2bd6412ddc65bda54909323199cb7e010fc3ebd9
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-13 14:04:37 -0500

    🛠 BUILD: CRA + general reset

---
+++ reducer-todo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Reducer Pattern Todo App</h1>
+    </div>
+  );
+}
+
+export default App;
